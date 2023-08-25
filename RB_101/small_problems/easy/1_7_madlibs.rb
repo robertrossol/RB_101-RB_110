c9188fffb8373088c075d59e539bcdc774e19cad
Robert Rossol <rrossol88@gmail.com>
2023-08-25 13:42:48 -0500

    madlibs exercise completed

---
+++ RB_101/small_problems/easy/1_7_madlibs.rb
@@ -0,0 +1,32 @@
+# Mad libs are a simple game where you create a story template with blanks for words. You, or another player, then construct a list of words and place them into the story, creating an often silly or funny story as a result.
+
+# Create a simple mad-lib program that prompts for a noun, a verb, an adverb, and an adjective and injects those into a story that you create.
+
+# Example
+
+# Enter a noun: dog
+# Enter a verb: walk
+# Enter an adjective: blue
+# Enter an adverb: quickly
+
+# Do you walk your blue dog quickly? That's hilarious!
+
+def madlib
+  # puts "Enter a noun:"
+  # noun = gets.chomp
+  inputs = {
+              "noun" => "",
+              "verb" => "",
+              "adjective" => "",
+              "adverb" => "",
+  }
+
+  inputs.keys.each do |x|
+    puts "Enter a #{x}:"
+    inputs[x] = gets.chomp
+  end
+
+  puts "Do you #{inputs["verb"]} your #{inputs["adjective"]} #{inputs["noun"]} #{inputs["adverb"]}? That's hilarious!"
+end
+
+madlib
