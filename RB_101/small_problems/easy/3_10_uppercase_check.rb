b65ecb811b144d529c7a119d052270d26aa97210
Robert Rossol <rrossol88@gmail.com>
2023-08-26 13:29:57 -0500

    uppercase check exercise complete

---
+++ RB_101/small_problems/easy/3_10_uppercase_check.rb
@@ -0,0 +1,12 @@
+# Write a method that takes a string argument, and returns true if all of the alphabetic characters inside the string are uppercase, false otherwise. Characters that are not alphabetic should be ignored.
+
+def uppercase?(text)
+  text == text.upcase
+end
+
+puts uppercase?('t') == false
+puts uppercase?('T') == true
+puts uppercase?('Four Score') == false
+puts uppercase?('FOUR SCORE') == true
+puts uppercase?('4SCORE!') == true
+puts uppercase?('') == true
